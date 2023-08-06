2ea4dc790f424d18aa6965289cc95077a2e61ebb
ALEX <ALEX@example.com>
2023-08-06 17:22:00 +0300

    Create programm

---
+++ Task08_HW/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Напишите программу, которая на вход принимает число (N),
+а на выходе показывает все чётные числа от 1 до N. */
+Console.WriteLine("Введите целое число");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+int number_2 = 2;
+
+while (number_2 < number_1)
+{
+Console.WriteLine(number_2);
+number_2 = number_2 + 2;
+}
