fe517cc5fa6acbc0488bc9bb1f83869c1e27f354
ALEX <ALEX@example.com>
2023-08-16 22:56:04 +0300

    Create Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая принимает на
+вход два числа и проверяет, является ли одно
+число квадратом другого.
+5, 25 -> да
+-4, 16 -> да
+25, 5 -> да
+8,9 -> нет */
+
+bool CheckSquareTwoNumbers(int num1, int num2)
+{
+ return num1 * num1 == num2 || num2 * num2 == num1;
+}
+Console.WriteLine("Please input two numbers");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = CheckSquareTwoNumbers(num1, num2);
+Console.WriteLine(result? "Yes" : "No");
