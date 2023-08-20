ead2088a98622cce5ab226aec3421a0c316c6b87
ALEX <ALEX@example.com>
2023-08-20 18:41:41 +0300

    Create Programm

---
+++ Task11_M/Program.cs
@@ -0,0 +1,17 @@
+﻿// Наипишите программу, которая выводит случайное трехзначное число
+// и удаляет вторую цифру этого числа.
+
+int DeleteSecondDigit(int num)
+{
+ int FirstDigit = num / 100;
+ int ThirdDigit = num % 10;
+ int result = FirstDigit * 10 + ThirdDigit;
+ return result;
+}
+
+int number = new Random().Next(100, 1000);
+int NewNumber = DeleteSecondDigit(number);
+
+//Console.WriteLine($"{NewNumber}");
+
+Console.WriteLine($"Дано: {number}. Ответ: {NewNumber}");
