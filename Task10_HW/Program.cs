3edd126a8225a4749899b77c44567345d75ebe9d
ALEX <ALEX@example.com>
2023-08-20 18:53:36 +0300

    Create Programm

---
+++ Task10_HW/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное
+// число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int ShowSecondDigit(int num)
+{
+ int FirstDigit = num / 100;
+ int ThirdDigit = num % 10;
+ int result = (num - (FirstDigit * 100 + ThirdDigit))/10;
+ return result;
+}
+
+int number = new Random().Next(100, 1000);
+int NewNumber = ShowSecondDigit(number);
+
+//Console.WriteLine($"{NewNumber}");
+
+Console.WriteLine($"Дано: {number}. Ответ: {NewNumber}");
