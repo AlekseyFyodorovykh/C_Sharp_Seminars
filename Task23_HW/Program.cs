28bf82c581116712a3e2dd546426ea6657ddb270
ALEX <ALEX@example.com>
2023-08-30 23:47:33 +0300

    Create Task23_HW

---
+++ Task23_HW/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Внутри класса Answer напишите метод ShowCube,
+который принимает на вход число (N) и выводит таблицу
+кубов чисел от 1 до N (включительно) каждое на новой строке. */
+
+void ShowCube(int number)
+{
+    int i = 1;
+    while(i <= number)
+    {
+        Console.WriteLine($"{i, 3} -> {i*i*i, 5}");
+        i++;
+    }
+}
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+ShowCube(n);
