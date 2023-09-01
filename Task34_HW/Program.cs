a90d1103b21b7faa62efed1108205e57ac3796ef
ALEX <ALEX@example.com>
2023-09-01 19:38:11 +0300

    Create Task34

---
+++ Task34_HW/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Задайте массив, заполненный случайными положительными трехзначными числами.
+Напишите программу, которая покажет количество четных чисел в массиве. */
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+ int[] arr = new int[size];
+ Random rnd = new Random();
+
+ for (int i = 0; i < arr.Length; i++)
+ {
+  arr[i] = rnd.Next(min, max + 1);
+ }
+ return arr;
+}
+
+void PrintArray(int[] arr)
+{
+ Console.Write("[");
+ for (int i = 0; i < arr.Length; i++)
+ {
+  if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+  else Console.Write($"{arr[i]}");
+ }
+ Console.Write("]");
+}
+
+int EvenElem(int[] arr)
+{
+ int count = 0;
+ for (int i = 0; i < arr.Length; i++)
+ {
+  if (arr[i] % 2 == 0) count++;
+ }
+ return count;
+}
+
+int[] array = CreateArrayRndInt(8, 100, 1000);
+PrintArray(array);
+Console.WriteLine();
+int elemEven = EvenElem(array);
+Console.WriteLine($"Количество четных чисел в массиве {elemEven}");
+
