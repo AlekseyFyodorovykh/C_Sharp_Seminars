0d30e66a19da190ce3c9cc13871ad2f92bef7b83
ALEX <ALEX@example.com>
2023-09-01 16:59:53 +0300

    Create Task27

---
+++ Task27_HW/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая принимает на вход число
+и выдает сумму цифр в числе */
+
+int SummOfDigitCount(int num)
+{
+ int DigitCount = 0;
+ while (num != 0)
+ {
+  DigitCount = DigitCount + num % 10;
+  num = num / 10;
+ }
+ return DigitCount;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма цифр числа {number} = {SummOfDigitCount(number)}");
