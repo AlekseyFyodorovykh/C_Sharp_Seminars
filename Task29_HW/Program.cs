badf698ce203ea3c28d266a92c6a1c4f5f6afc62
ALEX <ALEX@example.com>
2023-09-01 17:54:55 +0300

    Create Task29

---
+++ Task29_HW/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 29: напишите программу, которая
+задает массив из 8 элементов, заполненный
+псевдослучайными числами и выводит их на экран */
+
+
+void FillArray(int[] collection)
+{
+ int length = collection.Length;
+ int index = 0;
+ while (index < length)
+ {
+  collection[index] = new Random().Next();
+  index++;
+ }
+}
+
+void PrintArray(int[] col)
+{
+ int count = col.Length;
+ int position = 0;
+ while (position < count)
+ {
+  //Console.WriteLine(col[position]); //вывод в столбец
+  Console.Write(col[position] + " "); // вывод в строку
+  position++;
+ }
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
+
+
